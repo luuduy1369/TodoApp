60cf520815cf3c0b96f4a12d76bdd4be28a67ef4
luuduy1369 <khanhduyluu@gmail.com>
2019-05-24 20:41:24 +0700

    Convert to MVC design

---
+++ TodoApp/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TodoApp
+//
+//  Created by pro on 5/24/19.
+//  Copyright © 2019 LUUKHANHDUY. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
